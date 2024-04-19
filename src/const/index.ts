719d214527a4a89c68a2ac4d9034298c51fe7a74
joseavilasg <joseavilasg.dev@gmail.com>
2024-04-19 00:55:52 -0500

    Update email

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,5 +1,5 @@
 export const SOCIALS_URL = {
    LINKEDIN: "https://www.linkedin.com/in/joseavilasg",
    GITHUB: "https://github.com/joseavilasg",
-   EMAIL: "mailto:joseavilasg.dev@gmail.com",
+   EMAIL: "mailto:aviladiazjose09@gmail.com",
 }
